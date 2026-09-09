dc79b8f9a0d3d718b03e3adb99ffd48735dacba8
Diogo Tavares <dp.tavares05@gmail.com>
2026-09-09 13:34:20 +0100

    fixed handleEditClick fucntion in working but suboptimal way and also changed the hardcoded edit button to a value that can be changed

--- projects/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ projects/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -6,13 +6,13 @@ export default function Player({name, symbol}) {
     const[isEditing, setIsEditing] = useState(false);
 
     function handleEditClick(clickedButton){
-        setIsEditing(true); 
+        setIsEditing(!isEditing); 
     }
     
     let playerName = <span className="player-name">{name}</span>;
-    
+    // let buttonCaption = 'Save'
     if (isEditing) {
-        playerName = <input type="text" required/>
+        playerName = <input type="text" required value={name}/>
     }
     return(
           <li>
@@ -20,7 +20,7 @@ export default function Player({name, symbol}) {
               {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'} </button>
           </li>
     );
 }
