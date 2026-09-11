2eea4118754a852eb0f54625d9bd411bb4045249
Diogo Tavares <dp.tavares05@gmail.com>
2026-09-11 21:42:08 +0100

    created the main GameBoard component with only hardcoded values but a defined optimal structure

--- projects/07-tic-tac-toe-starting-project/src/App.jsx
+++ projects/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,8 +1,7 @@
+import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
@@ -10,8 +9,9 @@ function App() {
           <Player initialName ="Player 1" symbol="X"/>
           <Player initialName ="Player 2" symbol="O"/>
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
+      LOG
     </main>
   )
 }
