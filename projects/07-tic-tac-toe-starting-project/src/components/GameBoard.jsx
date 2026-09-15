93306fd98eaeda023f256b639835950ccb7a4c65
Diogo Tavares <dp.tavares05@gmail.com>
2026-09-15 12:52:46 +0100

    disabled squares conditionally if they have already been chosen

--- projects/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ projects/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -34,7 +34,12 @@ function handleSelectSquare(rowIndex, colIndex){
             <ol>
               {row.map((playerSymbol,colIndex) => (
                 <li key={colIndex}>
-                  <button onClick={() => onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                  <button 
+                    onClick={() => onSelectSquare(rowIndex,colIndex)}
+                    disabled = {playerSymbol !== null}
+                  >
+                    {playerSymbol}
+                  </button>
                 </li>
               ))}
             </ol>
